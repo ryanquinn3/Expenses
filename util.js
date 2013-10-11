b08949b3bd3c8bb4cb41d0e0b58e3aac13c33390
Ryan Quinn <ryan3q@gmail.com>
2013-10-11 15:26:43 -0400

    text field

--- util.js
+++ util.js
@@ -14,26 +14,38 @@ this.bind("add", options.view.addExpenseLi);
 });
 
 AppView = Backbone.View.extend({
-el: $("body"),
-initialize: function () {
-this.expenses = new Expenses( null, { view: this });
-//Create a friends collection when the view is initialized.
-//Pass it a reference to this view to create a connection between the two
-},
-events: {
-"click #add-expense":  "showPrompt",
-},
-showPrompt: function () {
-var expense_amount = prompt("What is the amount?");
-var expense_model = new Expense({ name: "$"+expense_amount});
-//Add a new friend model to our friend collection
-this.expenses.add( expense_model );
-},
-addExpenseLi: function (model) {
-//The parameter passed is a reference to the model that was added
-$("#expense-list").append("<li>" + model.get('name') + "</li>");
-//Use .get to receive attributes of the model
-}
+	el: $("body"),
+	initialize: function () {
+		this.expenses = new Expenses( null, { view: this });
+		
+	
+	},
+	events: {
+		"keypress #add-expense":  "showPrompt",
+	},
+	showPrompt: function (e) {
+
+		var input = $("#add-expense");
+		if (e.keyCode != 13) return;
+     	if (!input.val()) return;
+		if(input.val().charAt(0) === "$")
+		{
+			var correct =  input.val();
+		}
+		else
+		{
+			var correct = "$"+ input.val();
+		}
+		var expense_model = new Expense({ name: correct});
+		//Add a new friend model to our friend collection
+		this.expenses.add( expense_model );
+		input.val("");
+	},
+	addExpenseLi: function (model) {
+	//The parameter passed is a reference to the model that was added
+	$("#expense-list").append("<li>" + model.get('name') + "</li>");
+	//Use .get to receive attributes of the model
+	}
 });
 
 var appview = new AppView;
