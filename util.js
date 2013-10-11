029df097e07fab0655bbbdf2fcbd560b6dff983f
GSchill25 <schill1025@gmail.com>
2013-10-11 14:23:57 -0400

    working add friend

--- util.js
+++ util.js
@@ -1,6 +1,12 @@
-var temp = function(){
-	
-}
-var testPush = function(){
-	
-}
+(function ($) {
+window.AppView = Backbone.View.extend({
+  el: $("body"),
+  events: {
+    "click #add-friend":  "showPrompt",
+  },
+  showPrompt: function () {
+    var friend_name = prompt("Who is your friend?");
+  }
+});
+var appview = new AppView;
+})(jQuery);
